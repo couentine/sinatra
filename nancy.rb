57ce1ff74054a94ad3fa17a25e6dbb6547d5648c
Quentin <quent.guillaume@gmail.com>
2018-02-21 14:10:47 -0800

    finalize Sinatra

--- nancy.rb
+++ nancy.rb
@@ -12,20 +12,42 @@ module Nancy
       route("GET",path,&handler)
     end
 
+    def post(path, &handler)
+      route("POST", path, &handler)
+    end
+
+    def put(path, &handler)
+      route("PUT", path, &handler)
+    end
+
+    def patch(path, &handler)
+      route("PATCH", path, &handler)
+    end
+
+    def delete(path, &handler)
+      route("DELETE", path, &handler)
+    end
+
     def call(env)
       @request = Rack::Request.new(env)
       verb = @request.request_method
       requested_path = @request.path_info
 
-     handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
+      handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
 
-       if handler
-        instance_eval(&handler)
-       else
+      if handler
+        result = instance_eval(&handler)
+        if result.class == String
+          [200, {}, [result]]
+        else
+          result
+        end
+      else
         [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
-     end
+      end
     end
 
+    attr_reader :request
 
     private
     def route(verb,path,&handler)
@@ -37,18 +59,23 @@ module Nancy
       @request.params
     end
   end
+
+
+Application = Base.new
+
+module Delegator
+   def self.delegate(*methods, to:)
+     Array(methods).each do |method_name|
+       define_method(method_name) do |*args, &block|
+         to.send(method_name, *args, &block)
+       end
+
+       private method_name
+     end
+   end
+
+   delegate :get, :patch, :put, :post, :delete, :head, to: Application
+ end
 end
 
-nancy = Nancy::Base.new
-
-nancy.get'/hello' do
-[200,{}, ["Nancy says hello"]]
-end
-
-nancy.get '/' do
-  [200, {}, ["Your params are #{params.inspect}"]]
-end
-
-Rack::Handler::WEBrick.run nancy, Port: 9292
-
-puts nancy.routes
+ include Nancy::Delegator
