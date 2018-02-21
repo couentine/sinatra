37070906789372aed03fb3ae596ad3855f2ae436
Quentin <quent.guillaume@gmail.com>
2018-02-21 11:17:37 -0800

    add params and 404

--- nancy.rb
+++ nancy.rb
@@ -12,10 +12,29 @@ module Nancy
       route("GET",path,&handler)
     end
 
+    def call(env)
+      @request = Rack::Request.new(env)
+      verb = @request.request_method
+      requested_path = @request.path_info
+
+     handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
+
+       if handler
+        instance_eval(&handler)
+       else
+        [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
+     end
+    end
+
+
     private
     def route(verb,path,&handler)
       @routes[verb] ||= {}
       @routes[verb][path] = handler
+    end
+
+    def params
+      @request.params
     end
   end
 end
@@ -25,4 +44,11 @@ nancy = Nancy::Base.new
 nancy.get'/hello' do
 [200,{}, ["Nancy says hello"]]
 end
+
+nancy.get '/' do
+  [200, {}, ["Your params are #{params.inspect}"]]
+end
+
+Rack::Handler::WEBrick.run nancy, Port: 9292
+
 puts nancy.routes
